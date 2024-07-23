6d8ec911bed38783bb414ad11283336f3cac5e99
Kilian Birkhoven <kilian.birkhoven@sap.com>
2024-07-23 20:25:06 +0200

    oops

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
-cd /Users/i589221/Documents/BigMap/maegges/frontend
+cd ./maegges/frontend
 npm i
 cd ../backend
 python3  -m venv ./.venv
 source ./.venv/bin/activate
 pip install -r requirements.txt
-python main.py
